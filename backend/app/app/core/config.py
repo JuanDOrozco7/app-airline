3e382a6bfbca08f4628447158faeea1eda30c640
juandoro <juanorozcodc@gmail.com>
2022-05-06 23:16:09 -0500

    Cambios para imagen Azure

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -3,13 +3,13 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
 
-    DATABASE_USERNAME : str = 'postgres'
+    DATABASE_USERNAME : str = 'adminjd'
     DATABASE_PASSWORD : str = '123123'
-    DATABASE_HOST : str = 'localhost'
+    DATABASE_HOST : str = 'fastapi-db-airline-jd.postgres.database.azure.com'
     DATABASE_PORT : str = '6548'
-    DATABASE_NAME : str = 'mydb'
+    DATABASE_NAME : str = 'myairlineapp'
 
-    DATABASE_URI : str = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    DATABASE_URI : str = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
     ACCESS_TOKEN_EXPIRE_MINUTES : int = 60 * 24 * 5  # 60 * 24 * 5 = 5 days
     SECRET_KEY : str = '5ZhRhFEwqLrIwGdc11bpzWPlP24Xwc6n'
     ALGORITHM : str = "HS512"
